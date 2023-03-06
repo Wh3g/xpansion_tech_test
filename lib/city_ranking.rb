1d6677ba86653480c499311307dce3d551f7f3ce
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-03-06 17:02:50 +0000

    refactored #cities_scores into own method

--- lib/city_ranking.rb
+++ lib/city_ranking.rb
@@ -7,12 +7,16 @@ class CityRanking
         response = HTTP.get("https://api.teleport.org/api/continents/geonames%3A#{code}/urban_areas/")
         @cities = response.parse["_links"]["ua:items"]
 
+        cities_scores
+    end
+
+    private 
+    
+    def cities_scores
         @cities.each do | city |
             response = HTTP.get(city["href"] + "scores/")
             city["teleport_city_score"] = response.parse["teleport_city_score"]
             city["summary"] = response.parse["summary"]
         end
     end
-
-
 end
