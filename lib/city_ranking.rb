ea1926aa575f1cdc85598a40714b7c3555399e5c
Willoughby Hood <willoughbyhood29@googlemail.com>
2023-03-06 16:58:04 +0000

    #cities saves the scores and summary for each city

--- lib/city_ranking.rb
+++ lib/city_ranking.rb
@@ -5,8 +5,13 @@ class CityRanking
 
     def continent_select(code)
         response = HTTP.get("https://api.teleport.org/api/continents/geonames%3A#{code}/urban_areas/")
-        hash = response.parse["_links"]
-        @cities = hash["ua:items"]
+        @cities = response.parse["_links"]["ua:items"]
+
+        @cities.each do | city |
+            response = HTTP.get(city["href"] + "scores/")
+            city["teleport_city_score"] = response.parse["teleport_city_score"]
+            city["summary"] = response.parse["summary"]
+        end
     end
 
 
--- spec/city_ranking_spec.rb
+++ spec/city_ranking_spec.rb
@@ -2,11 +2,20 @@ require 'city_ranking'
 
 describe CityRanking do
     describe "#continent_select" do
-        it "returns list of cities in continent" do
+        it "saves list of cities in continent" do
             subject.continent_select("EU")
-            expect(subject.cities).to include({ "href" => "https://api.teleport.org/api/urban_areas/slug:london/", "name" => "London" })
+            expect(subject.cities).to include(include("name" => "London" ))
+        end
+
+        it "you can select the continent with the code variable" do
             subject.continent_select("AF")
-            expect(subject.cities).to include({ "href" => "https://api.teleport.org/api/urban_areas/slug:cairo/", "name" => "Cairo"})
+            expect(subject.cities).to include(include("name" => "Cairo" ))
+        end
+
+        it "saves the scores and summary for each city" do
+            subject.continent_select("EU")
+            expect(subject.cities.first).to have_key("teleport_city_score")
+            expect(subject.cities.first).to have_key("summary")
         end
     end
 end
